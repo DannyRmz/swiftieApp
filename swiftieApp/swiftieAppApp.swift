b44410e2b09f7c7660470054a2df9c0e795ee708
Daniel Ramirez <daniel.ramirezm@ingenieria.unam.edu>
2026-03-03 13:07:26 -0600

    first commit

---
+++ swiftieApp/swiftieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftieAppApp.swift
+//  swiftieApp
+//
+//  Created by Daniel Ramirez on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
